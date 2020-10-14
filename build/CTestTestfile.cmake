865f50b2b38fb07803a6b2876deb3ea68bda5c18
Кузнецов Александр Анатольевич <kuznecov@triton.e-burg.ru>
2020-10-14 08:44:06 +0500

    First commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/kuznecov/Desktop/SKFGtest
+# Build directory: C:/Users/kuznecov/Desktop/SKFGtest/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
